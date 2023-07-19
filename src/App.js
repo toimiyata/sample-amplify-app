875f2ae6ab35c44ee3ae63aee40e825be1d751a3
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-19 11:23:41 +0900

    Authenticatorコンポーネントで実装

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      <div className= "App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
