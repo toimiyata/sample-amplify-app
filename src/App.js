40015fd9ee02821b7fed33b771921d49270bcf0d
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-19 14:18:58 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Amplify, Auth } from 'aws-amplify';
-import '@aws-amplify/ui-react/styles.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
@@ -10,12 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sample React app</h1>
-        <h2>
-          <a className="App-link" href="."
+        <h2><a className="App-link" href="."
           onClick={Auth.signOut}>
           signOut
-          </a>
-        </h2>
+        </a></h2>
       </header>
     </div>
   );
