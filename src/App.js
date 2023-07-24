eab09a073ac7fb15e9a18cbeba39c3cd25d66549
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-24 18:21:36 +0900

    figmaで作成したHeaderコンポーネントを使用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
-import { Auth } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
+import { Header } from './ui-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2><a className="App-link" href="."
-          onClick={Auth.signOut}>
-          signout
-        </a></h2>
-      </header>
+    <div className="py-4">
+      <Header className="mb-4">
+        <p>※これは、UIコンポーネントを利用した表示です。</p>
+      </Header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AmplifyProvider } from "@aws-amplify/ui-react";
+import awsconfig from '../../src/aws-exports';
+import { Amplify } from 'aws-amplify';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
