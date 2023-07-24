2886f838ece9e3cb6f05e32ecec9307ddb52b37c
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-24 18:29:18 +0900

    aws-exportsの位置をsrc内に変更

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AmplifyProvider } from "@aws-amplify/ui-react";
-import awsconfig from '../../src/aws-exports';
+import awsconfig from './aws-exports';
 import { Amplify } from 'aws-amplify';
 import reportWebVitals from './reportWebVitals';
 
