e70d90b75adcf27ccc9dfdbc5d88503c9ee85c81
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-25 18:31:04 +0900

    コンポーネントの子要素

--- src/App.js
+++ src/App.js
@@ -5,52 +5,30 @@ import { Header } from './ui-components';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-function AlertMessage(props) {
+function Messaege(props) {
   return (
-    <div className='alert alert-primary'>
-      <h3>{props.title}</h3>
-      {props.msg}
-    </div>
-  )
-}
-
-function BoxMessage(props) {
-  return (
-    <div className='card'>
-      <div className='card-header'>
-        {props.title}
-      </div>
-      <div className='card-body'>
-        {props.msg}
-      </div>
+    <div className={"alert alert-" + props.type}>
+      {props.children}
     </div>
   )
 }
 
 function App() {
-  const [flg, setFlg] = useState(false);
- 
-  const onChange = (event)=> {
-    setFlg(event.target.checked);
-  }
-
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
-      <div className='form-check'>
-        <input type="checkbox" className='form-check-input' id="check1"
-          onChange={onChange}/>
-        <label className='form-check-label' htmlFor='check1'>
-          表示の切り替えチェックボックス
-        </label>
-      </div>
-      <hr />
-      {flg ?
-        <AlertMessage title="チェックはON!!" msg="チェックはONのメッセージ"/>
-      :
-        <BoxMessage title="チェックはOFF" msg="チェックはOFFのメッセージ"/>
-      }   
+      <Messaege type = "dark">
+        <h1>メッセージ</h1>
+        <p>これはMessageコンポーネントの利用例です。</p>
+      </Messaege>
+      <Messaege type="info">
+        <ul>
+          <li>メッセージ１</li>
+          <li>メッセージ２</li>
+          <li>メッセージ３</li>
+        </ul>
+      </Messaege>
     </div>
   );
 }
