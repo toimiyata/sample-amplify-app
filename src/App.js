df214f3411195c35925a6db77fdb435623490da8
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-25 14:05:51 +0900

    ステートの利用

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Header } from './ui-components';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = ()=> {
+    setCount(count + 1);
+  }
+
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
+      <div className='alert alert-primary'>
+        count: {count};
+      </div>
       <button className='btn btn-primary' onClick={onClick}>
         Click me!
       </button>
     </div>
   );
-}
-
-function onClick() {
-  alert("クリックした！");
 }
 
 export default withAuthenticator(App);
