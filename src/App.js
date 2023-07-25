73977995765acbe784f28dc21f876548bdfa7a77
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-25 13:27:05 +0900

    コンポーネントの追加

--- src/App.js
+++ src/App.js
@@ -8,8 +8,28 @@ function App() {
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
+      <Hello />
+      <Now />
     </div>
   );
+}
+
+function Hello() {
+  return (
+    <p className='border border-primary p-3 my-3'>
+      こんにちは
+    </p>
+  )
+}
+
+function Now() {
+  return (
+    <p className='bg-secondary text-dark bg-opacity-25 p-3 my-3'>
+      現在は、{
+        new Date().getHours()
+      }時です。
+    </p>
+  )
 }
 
 export default withAuthenticator(App);
