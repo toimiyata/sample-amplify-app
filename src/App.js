3a5be44e69865b3c3a70eab31ba5fe2bac3a6f84
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-25 14:27:07 +0900

    ステートをコンポーネントの属性として渡す

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Header } from './ui-components';
 import { useState } from 'react';
 
+function Hello(props){
+  return (
+    <div className={'alert alert-' + props.type}>
+      {props.message}
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [msg, setMsg] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  
+  const onChange = (event)=> {
+    setMsg(event.target.value);
+  }
+
   const onClick = ()=> {
-    setCount(count + 1);
+    setMsgs([
+      "Hello, " + msg + "!",
+      "こんにちは　" + msg + "!"
+    ]);
   }
 
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
-      <div className='alert alert-primary'>
-        count: {count};
+      <div className='mx-0 my-3 row'>
+        <input type="text" className='form-control col'
+          onChange={onChange}/>
+        <button className='btn btn-primary col-2'
+          onClick={onclick}/>
       </div>
-      <button className='btn btn-primary' onClick={onClick}>
-        Click me!
-      </button>
+      <Hello message={msgs[0]} type="primary" />
+      <Hello message={msgs[1]} type="dark" />
     </div>
   );
 }
