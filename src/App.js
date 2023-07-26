0465bc0e5fefa2bea90730cb88593849e0be7134
TOI 宮田 <da.miyata@toi-iot.net>
2023-07-26 11:12:36 +0900

    UIコンポーネント配置の準備

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
+import { Auth } from 'aws-amplify'
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { Header } from './ui-components';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
-function Messaege(props) {
-  let first = null;
-  let data = null;
-  if (Array.isArray(props.children)) {
-    first = props.children[0];
-    data = props.children.slice(1, props.children.length);
-  } else {
-    first = props.children;
-    data = [<p>no data</p>]
-  }
-  return (
-    <div className={"alert alert-primary"}>
-    <ul className='list-group'>
-      <div className='text-center'>{first}</div>
-      {data.map(value => (
-        <li className='list-group-item'>
-          {value}
-        </li>
-      ))}
-    </ul>  
-    </div>
-  )
-}
+const content1 = <p>タブ1のコンテンツ</p>;
+const content2 = <p>タブ2のコンテンツ</p>;
+const content3 = <p>タブ3のコンテンツ</p>;
+const content4 = <p>タブ4のコンテンツ</p>;
 
 function App() {
   return (
     <div className="py-4">
       <Header className="mb-4" />
       <p>※これは、UIコンポーネントを利用した表示です。</p>
-      <Messaege>
-        <p>タイトル</p>
-        <p>サンプルメッセージ１</p>
-        <p>サンプルメッセージ２</p>
-      </Messaege>
-    </div>
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <a href='#tab1' className='nav-link active'
+            data-bs-toggle="tab">List</a>
+        </li>
+        <li className='nav-item'>
+          <a href='#tab2' className='nav-link'
+            data-bs-toggle="tab">Create</a>
+        </li>
+        <li className='nav-item'>
+          <a href='#tab3' className='nav-link'
+            data-bs-toggle="tab">Update</a>
+        </li>
+        <li className='nav-item'>
+          <a href='#tab4' className='nav-link'
+            data-bs-toggle="tab">Delete</a>
+        </li>
+      </ul>
+      <div className='tab-content'>
+        <div id="tab1" className='my-2 tab-pane active'>
+          { content1 }
+        </div>
+      <div id="tab2" className='my-2 tab-pane'>
+          { content2 }
+        </div>
+      </div>
+      <div className='tab-content'>
+        <div id="tab3" className='my-2 tab-pane'>
+          { content3 }
+        </div>
+      <div id="tab4" className='my-2 tab-pane'>
+          { content4 }
+        </div>
+      </div>
+    <p className='my-2'>
+      <a className='btn btn-primary' href="."
+      onClick={Auth.sighOut}></a>
+    </p>
+   </div>
   );
 }
 
